e918737e44c2c9e297654d466b13add30c3c4498
Dhonaldduck <168738134+Dhonaldduck@users.noreply.github.com>
2024-12-06 22:04:00 +0700

    Tubes.h

--- Tubes.h
+++ Tubes.h
@@ -1,5 +1,12 @@
+#ifndef Header_h
+#define Header_h
+
 #include <iostream>
 using namespace std;
+
+typedef struct elm_Maskapai *adr_Maskapai;
+typedef struct elm_Rute *adr_Rute;
+typedef struct elm_Relation* adr_relasi;
 
 struct elm_Maskapai{
     infotype infoMaskapai;
@@ -12,7 +19,8 @@ struct elm_Rute {
     adr_Rute next;
 };
 struct elm_Relation {
-    infotype infoRelation;
+    adr_Maskapai parent;
+    adr_Rute child;
     adr_relasi next;
 };
 
@@ -30,10 +38,6 @@ struct rutePenerbangan {
 
 };
 
-typedef struct elm_Maskapai *adr_Maskapai;
-typedef struct elm_Rute *adr_Rute;
-typedef struct elm_Relation* adr_relasi;
-
 struct List_Maskapai{
     adr_Maskapai first;
     adr_Maskapai last;
@@ -41,12 +45,11 @@ struct List_Maskapai{
 struct List_Rute{
     adr_Rute first;
 };
-
-struct List_Relation {
+struct List_Relation{
     adr_relasi first;
 };
-typedef rutePenerbangan infotypeRute;
 
+typedef rutePenerbangan infotypeRute;
 
 void createListMaskapai(List_Maskapai &Lm);
 void createListRute(List_Rute &LR);
@@ -54,15 +57,18 @@ void createListRelation(List_Relation &Rt);
 bool isEmptyRute(List_Rute LR);
 adr_Maskapai createElmMaskapai(infotype x);
 adr_Rute newElementRute(infotypeRute x);
-adr_relasi newElementRelation(infotype x);
+adr_relasi createElmRelation(adr_Maskapai parent, adr_Rute child);
 void insertMaskapai(List_Maskapai &Lm, adr_Maskapai P);
 void addRute(List_Rute &LR, adr_Rute p);
-void insertRelasi(List_Relation &Rt, adr_relasi P);
+void insertRelation(List_Relation &Rt, adr_Maskapai parent, adr_Rute child);
 void deleteMaskapai(List_Maskapai &Lm, adr_Maskapai &P);
 void deleteRute(List_Rute &LR, adr_Rute p);
-void deleteRelation(List_Relation &Rt, adr_relasi P)
+void deleteRelation(List_Relation &Rt, adr_Maskapai parent, adr_Rute child);
 string findMaskapai(List_Maskapai Lm, string kode);
 adr_Rute findRute(List_Rute LR, infotypeRute x);
-bool findRelasi(List_Relation RT, adr_rutePenerbangan P, adr_Maskapai Q);
+adr_relasi findRelation(List_Relation Rt, adr_Maskapai parent, adr_Rute child);
 void showMaskapai(List_Maskapai Lm);
 void printInfoRute(List_Rute LR);
+void showRelations(List_Relation Rt);
+
+#endif /* Header_h */
