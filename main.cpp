0f7f7d87cac780043814acf479c6a7fe6e41e598
Jundihd <amang.udinjundi@gmail.com>
2024-12-15 22:41:06 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Header.h"
+#include "Penerbangan.h"
 using namespace std;
 
 int main() {
@@ -7,36 +7,33 @@ int main() {
     List_Rute LR;
     createListMaskapai(LM);
     createListRute(LR);
-    
     int choice;
-    string kodeMaskapai, kodeRute;
-    infotypeMaskapai maskapai;
-    infotypeRute rute;
-
-    do {
-        // Menu Utama
-        cout << "\nMenu Utama:" << endl;
-        cout << "1. Tambah Maskapai" << endl;
-        cout << "2. Tambah Rute" << endl;
-        cout << "3. Tambah Relasi Maskapai dan Rute" << endl;
-        cout << "4. Hapus Maskapai" << endl;
-        cout << "5. Hapus Rute" << endl;
-        cout << "6. Tampilkan Maskapai" << endl;
-        cout << "7. Tampilkan Rute" << endl;
-        cout << "8. Tampilkan Rute dari Maskapai" << endl;
-        cout << "9. Tampilkan Maskapai dari Rute" << endl;
-        cout << "10. Tampilkan Maskapai dan Rute" << endl;
-        cout << "11. Edit Rute dari Maskapai" << endl;
-        cout << "12. Hitung Relasi Maskapai" << endl;
-        cout << "13. Hitung Relasi Rute" << endl;
-        cout << "14. Hitung Rute yang tidak memiliki Relasi" << endl;
-        cout << "0. Keluar" << endl;
-        cout << "Pilih menu (0-14): ";
-        cin >> choice;
+    cout << "\nMenu Utama:" << endl;
+    cout << "1. Tambah Maskapai" << endl;
+    cout << "2. Tambah Rute" << endl;
+    cout << "3. Cari Maskapai" << endl;
+    cout << "4. Cari Rute" << endl;
+    cout << "5. Tambah Relasi Maskapai dan Rute" << endl;
+    cout << "6. Hapus Maskapai" << endl;
+    cout << "7. Hapus Rute" << endl;
+    cout << "8. Tampilkan Maskapai" << endl;
+    cout << "9. Tampilkan Rute" << endl;
+    cout << "10. Tampilkan Rute dari Maskapai" << endl;
+    cout << "11. Tampilkan Maskapai dari Rute" << endl;
+    cout << "12. Tampilkan setiap Rute dan Maskapainya" << endl;
+    cout << "13. Tampilkan setiap data Maskapai dan Relasinya" << endl;
+    cout << "14. Hitung Relasi Maskapai" << endl;
+    cout << "15. Hitung Relasi Rute" << endl;
+    cout << "16. Hitung Rute yang tidak memiliki Relasi" << endl;
+    cout << "17. Edit Rute dari Maskapai" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "Pilih menu (0-15): ";
+    cin >> choice;
 
         switch (choice) {
             case 1: {
                 // Menambahkan Maskapai
+                infotypeMaskapai maskapai;
                 cout << "Masukkan Nama Maskapai: ";
                 cin >> maskapai.namaMaskapai;
                 cout << "Masukkan Kode Maskapai: ";
@@ -54,6 +51,7 @@ int main() {
 
             case 2: {
                 // Menambahkan Rute
+                infotypeRute rute;
                 cout << "Masukkan Kota Asal: ";
                 cin >> rute.kota_asal;
                 cout << "Masukkan Kota Tujuan: ";
@@ -68,15 +66,30 @@ int main() {
                 cout << "Rute berhasil ditambahkan!" << endl;
                 break;
             }
+            case 3:{
+                //Mencari Maskapai
+                string kodeMaskapai;
+                cin >> kodeMaskapai;
+                findMaskapai(LM,kodeMaskapai);
+                break;
+            }
+            case 4:{
+                //Mencari Rute
+                string kodeRute;
+                cin >> kodeRute;
+                findRute(LR,kodeRute);
+                break;
+            }
 
-            case 3: {
+            case 5: {
                 // Menambahkan Relasi antara Maskapai dan Rute
+                string kodeMaskapai,kodeRute;
                 cout << "Masukkan Kode Maskapai: ";
                 cin >> kodeMaskapai;
                 adr_Maskapai maskapaiRelasi = findMaskapai(LM, kodeMaskapai);
                 cout << "Masukkan Kode Penerbangan: ";
                 cin >> kodeRute;
-                adr_Rute ruteRelasi = findRute(LR, {kodeRute});
+                adr_Rute ruteRelasi = findRute(LR, kodeRute);
                 
                 if (maskapaiRelasi != NULL && ruteRelasi != NULL) {
                     insertRelation(maskapaiRelasi, ruteRelasi);
@@ -87,115 +100,89 @@ int main() {
                 break;
             }
 
-            case 4: {
+            case 6: {
                 // Menghapus Maskapai
-                cout << "Masukkan Kode Maskapai yang akan dihapus: ";
-                cin >> kodeMaskapai;
-                adr_Maskapai maskapaiDelete = findMaskapai(LM, kodeMaskapai);
-                if (maskapaiDelete != NULL) {
-                    deleteMaskapai(LM, maskapaiDelete);
-                    cout << "Maskapai berhasil dihapus!" << endl;
-                } else {
-                    cout << "Maskapai tidak ditemukan!" << endl;
-                }
+                adr_Maskapai p;
+                deleteMaskapai(LM,p);
                 break;
             }
 
-            case 5: {
+            case 7: {
                 // Menghapus Rute
                 cout << "Masukkan Kode Penerbangan yang akan dihapus: ";
+                string kodeRute;
+                adr_Rute prec;
                 cin >> kodeRute;
                 adr_Rute ruteDelete = findRute(LR, {kodeRute});
-                if (ruteDelete != NULL) {
-                    deleteRute(LR, ruteDelete);
+                if (ruteDelete == LR.first) {
+                    delete_First(LR, ruteDelete);
                     cout << "Rute berhasil dihapus!" << endl;
-                } else {
+                } else if (ruteDelete ->next == NULL){
+                    delete_Last(LR,ruteDelete);
+                    cout << "Rute berhasil dihapus!" << endl;
+                }else if(ruteDelete != LR.first && ruteDelete ->next != NULL){
+                    delete_After(LR,prec,ruteDelete);
+                    cout << "Rute berhasil dihapus!" << endl;
+                }else{
                     cout << "Rute tidak ditemukan!" << endl;
                 }
                 break;
             }
 
-            case 6: {
+            case 8: {
                 // Menampilkan Maskapai
                 showMaskapai(LM);
                 break;
             }
 
-            case 7: {
+            case 9: {
                 // Menampilkan Rute
                 showRute(LR);
                 break;
             }
 
-            case 8: {
-                // Menampilkan Rute dari Maskapai
-                cout << "Masukkan Kode Maskapai: ";
-                cin >> kodeMaskapai;
-                adr_Maskapai maskapaiShow = findMaskapai(LM, kodeMaskapai);
-                if (maskapaiShow != NULL) {
-                    show_RutefromMaskapai(maskapaiShow);
-                } else {
-                    cout << "Maskapai tidak ditemukan!" << endl;
-                }
-                break;
-            }
-
-            case 9: {
-                // Menampilkan Maskapai dari Rute
-                cout << "Masukkan Kode Penerbangan: ";
-                cin >> kodeRute;
-                adr_Rute ruteShow = findRute(LR, {kodeRute});
-                if (ruteShow != NULL) {
-                    show_MaskapaifromRute(LM, ruteShow);
-                } else {
-                    cout << "Rute tidak ditemukan!" << endl;
-                }
-                break;
-            }
-
             case 10: {
-                // Menampilkan Maskapai dan Rute
-                show_MaskapaiAndRute(LM, LR);
+                // Menampilkan Rute dari Maskapai
+               adr_Maskapai P;
+               show_RutefromMaskapai(P,LM);
                 break;
             }
 
             case 11: {
-                // Mengedit Rute dari Maskapai
-                cout << "Masukkan Kode Maskapai: ";
-                cin >> kodeMaskapai;
-                cout << "Masukkan Kode Rute: ";
-                cin >> kodeRute;
-                edit_RuteFromMaskapai(LM, LR, kodeMaskapai, kodeRute);
+                // Menampilkan Maskapai dari Rute
+                adr_Maskapai P;
+                show_MaskapaifromRute(LM,P);
                 break;
             }
 
             case 12: {
-                // Menghitung Relasi Maskapai
-                cout << "Masukkan Kode Maskapai: ";
-                cin >> kodeMaskapai;
-                adr_Maskapai maskapaiCount = findMaskapai(LM, kodeMaskapai);
-                if (maskapaiCount != NULL) {
-                    countRelationMaskapai(LM, maskapaiCount);
-                } else {
-                    cout << "Maskapai tidak ditemukan!" << endl;
-                }
+                // Menampilkan setiap Rute beserta Maskapainya
+                show_MaskapaiAndRute(LM, LR);
                 break;
             }
 
             case 13: {
-                // Menghitung Relasi Rute
-                cout << "Masukkan Kode Penerbangan: ";
-                cin >> kodeRute;
-                adr_Rute ruteCount = findRute(LR, {kodeRute});
-                if (ruteCount != NULL) {
-                    countRelationRute(LM, ruteCount);
-                } else {
-                    cout << "Rute tidak ditemukan!" << endl;
-                }
+                // Menampilkan seluruh Maskapai dan Relasinya
+                adr_Rute P;
+                showAllDataRelasi(LM,P);
                 break;
             }
 
             case 14: {
+                // Menghitung Relasi Maskapai
+                adr_Maskapai P;
+               countRelationMaskapai(LM,P);
+                break;
+            }
+
+            case 15: {
+                // Menghitung Relasi Rute
+                adr_Rute P;
+                countRelationRute(LM,P);
+                break;
+            }
+
+            case 16: {
                 // Menghitung Rute yang tidak memiliki Relasi
                 int count = count_RuteDontHaveRelasi(LM, LR);
                 cout << "Jumlah rute yang tidak memiliki relasi: " << count << endl;
@@ -211,7 +198,5 @@ int main() {
                 break;
         }
 
-    } while (choice != 0);
+    }
 
-    return 0;
-}
